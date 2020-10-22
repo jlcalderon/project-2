365a917b6f00c5388fb286a9092e5c54c48d72f7
JLC <jlc@annonymous.com>
2020-10-21 21:41:21 -0500

    Fixing sintax errors in readme and apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,63 +15,55 @@ module.exports = function(app) {
         });
     });
 
-  });
 
-  //Update item from inventory request
-  app.put("/api/inventory/:id", function(req, res) {
-    db.inventory
-      .update(
-        {
-          categoryName: req.body.categoryName,
-          itemName: req.body.itemName,
-          quantity: req.body.quantity,
-          replenishFlag: req.body.replenishFlag,
-          price: req.body.price,
-          supplierName: req.body.supplierName
-        },
-        {
-          where: {
-            id: req.params.id
-          }
-        }
-      )
-      .then(function(dbInventory) {
-        res.json(dbInventory);
-      });
-  });
-  // Get all Listdetails
-  app.get("/api/listdetails", function(req, res) {
-    db.listdetails.findAll({}).then(function(dbGetlistdetails) {
-      res.json(dbGetlistdetails);
+    //Update item from inventory request
+    app.put("/api/inventory/:id", function(req, res) {
+        db.inventory
+            .update({
+                categoryName: req.body.categoryName,
+                itemName: req.body.itemName,
+                quantity: req.body.quantity,
+                replenishFlag: req.body.replenishFlag,
+                price: req.body.price,
+                supplierName: req.body.supplierName
+            }, {
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(function(dbInventory) {
+                res.json(dbInventory);
+            });
     });
-  });
-  // Create/Post a new Listdetails
-  app.post("/api/listdetails", function(req, res) {
-    db.listdetails.create(req.body).then(function(dbListdetails) {
-      res.json(dbListdetails);
+    // Get all Listdetails
+    app.get("/api/listdetails", function(req, res) {
+        db.listdetails.findAll({}).then(function(dbGetlistdetails) {
+            res.json(dbGetlistdetails);
+        });
     });
-  });
+    // Create/Post a new Listdetails
+    app.post("/api/listdetails", function(req, res) {
+        db.listdetails.create(req.body).then(function(dbListdetails) {
+            res.json(dbListdetails);
+        });
+    });
 
-  //Update item from Listdetails request
-  app.put("/api/listdetails/:idItem", function(req, res) {
-    console.log(req.params);
-    db.listdetails
-      .update(
-        {
-          quantityObtained: req.body.quantityObtained,
-          status: req.body.status
-        },
-        {
-          where: {
-            idItem: req.params.idItem
-          }
-        }
-      )
-      .then(function(dbListdetails) {
-        res.json(dbListdetails);
-      });
-  });
-};
+    //Update item from Listdetails request
+    app.put("/api/listdetails/:idItem", function(req, res) {
+        console.log(req.params);
+        db.listdetails
+            .update({
+                quantityObtained: req.body.quantityObtained,
+                status: req.body.status
+            }, {
+                where: {
+                    idItem: req.params.idItem
+                }
+            })
+            .then(function(dbListdetails) {
+                res.json(dbListdetails);
+            });
+    });
 
     // Find items running low on inventory
     app.get("/api/inventory/low", function(req, res) {
@@ -103,4 +95,3 @@ module.exports = function(app) {
             });
     });
 };
-
