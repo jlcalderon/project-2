9546d4e3c077ab28906d6789946ddd7e5c874687
JLC <jlc@annonymous.com>
2020-10-25 18:54:31 -0500

    Integration of update inventory and user privileges on dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    //*************************Add new Item to Inventory*************************//
     //Grab elements from DOM in variables
     const newItemForm = $(".add-inventory-form");
     const itemName = $("#item-name");
@@ -55,4 +57,50 @@ $(document).ready(function() {
             throw err;
         });
     }
+    //**************************Update Items from Inventory ******************************/
+    //When user with admin privileges click on update button
+    $(".update-item").on("click", function(event) {
+        event.preventDefault();
+        // Grabbing id of item clicked
+        let itemId = $(this).data("id");
+
+        //Create a JSON object from users input
+        //Not trimming values because there are some descriptions that are more than one word, it cant be trimmed
+        let updatedItem = {
+            categoryName: $(`#${itemId}-category`).val(),
+            itemName: $(`#${itemId}-name`).val(),
+            quantity: parseInt($(`#${itemId}-quantity`).val(), 10),
+            replenishFlag: parseInt($(`#${itemId}-replenish`).val(), 10),
+            price: parseFloat($(`#${itemId}-price`).val()),
+            supplierName: $(`#${itemId}-supplier`).val()
+        }
+
+        // Call in the function that updates our inventory item and pass on our object
+        updateItem(updatedItem);
+
+        function updateItem(itemObj) {
+            // Perform an Ajax request
+            console.log("Making PUT request");
+
+            $.ajax({
+                url: "api/inventory/" + itemId,
+                method: "PUT",
+                data: itemObj,
+            }).then(function(data) {
+                /* if succes here is next step */
+                /* do console log to see input record */
+                console.log(`updated item in inventory`);
+                /* Reload the page */
+                location.reload();
+            }).catch(function(err) {
+                /* if error throw the error */
+                throw err;
+            });
+        }
+    });
+
+
+
+    /************* */
+
 });
